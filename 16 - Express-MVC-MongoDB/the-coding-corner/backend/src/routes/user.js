31256f9c7d5322ec8df3e0a2bdb5b353857a7a59
Ace-Urriquia <aceroemelbradfordqurriquia@gmail.com>
2025-02-02 22:00:27 +0800

    done

--- 16 - Express-MVC-MongoDB/the-coding-corner/backend/src/routes/user.js
+++ 16 - Express-MVC-MongoDB/the-coding-corner/backend/src/routes/user.js
@@ -4,10 +4,10 @@ const router = express.Router();
 const {signupUser,loginUser} = require('../controllers/user');
 
 
-//POST: http://localhost:3000/users/signup
+//POST: http://localhost:3000/api/users/signup
 router.post("/signup",signupUser);
 
-//POST: http://localhost:3000/users/login
+//POST: http://localhost:3000/api/users/login
 router.post("/login",loginUser)
 
 
